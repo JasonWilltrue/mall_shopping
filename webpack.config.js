2bd7108c53700237c81e7ae16ddf4748f8957a06
JasonWilltrue <148373644@qq.com>
2018-11-07 13:40:02 +0800

    初步完成购物车模块开发

--- webpack.config.js
+++ webpack.config.js
@@ -30,16 +30,17 @@ module.exports = {
 	// entry: "./src/page/index/index.js",
 	entry: {
 		//通用模块
-		common: ['./src/page/common/index.js'],
+		'common': ['./src/page/common/index.js'],
 		'user-login': ['./src/page/user-login/index.js'],
 		'user-register': ['./src/page/user-register/index.js'],
 		'user-pass-reset': ['./src/page/user-pass-reset/index.js'],
 		'user-center': ['./src/page/user-center/index.js'],
 		'user-center-update': ['./src/page/user-center-update/index.js'],
-		index: ['./src/page/index/index.js'],
-		list: ['./src/page/list/index.js'],
-		detail: ['./src/page/detail/index.js'],
-		result: ['./src/page/result/index.js'],
+		'index': ['./src/page/index/index.js'],
+		'list': ['./src/page/list/index.js'],
+		'detail': ['./src/page/detail/index.js'],
+		'cart': ['./src/page/cart/index.js'],
+		'result': ['./src/page/result/index.js'],
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -135,6 +136,7 @@ module.exports = {
 		new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
 		new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人中心')),
 		new HtmlWebpackPlugin(getHtmlConfig('result', '搜索结果')),
+		new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
 		// new HtmlWebpackPlugin({
 		// 	template: './src/view/index.html',
 		// }),
